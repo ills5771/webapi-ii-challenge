394e11d3464fc38b79d3245f79e97753ad0362c9
ills5771 <ilyayelizarov1@gmail.com>
2019-03-19 13:36:47 -0700

    added put request endpoint

--- data/posts-router.js
+++ data/posts-router.js
@@ -69,5 +69,31 @@ router.delete("/:id", async (req, res) => {
     });
   }
 });
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+  if (post.title && post.contents) {
+    Posts.update(id, post)
+
+      .then(updated => {
+        if (updated) {
+          res.status(200).json(updated);
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          });
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+});
 
 module.exports = router;
