266d2c5198c9726fb99bfd00f54d4b7c643d2551
ills5771 <ilyayelizarov1@gmail.com>
2019-03-19 13:18:20 -0700

    added post endpoint

--- data/posts-router.js
+++ data/posts-router.js
@@ -34,4 +34,23 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  if (newPost.title && newPost.contents) {
+    Posts.insert(newPost)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+});
+
 module.exports = router;
